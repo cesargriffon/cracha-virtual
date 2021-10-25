b1b1cdadc0d71f9f56947b67a8022121c861e6ad
Cesar N. Silva <77632665+cesargriffon@users.noreply.github.com>
2021-10-25 11:30:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const LinksSocialMedia = {
+    github: "cesargriffon",
+    facebook: "cesar.nascimento.9400",
+    youtube: "cesargriffon",
+    instagram: "cesar.griffon",
+    twitter: "cesar.griffon"
+}
+
+function changeSocialMediaLinks(){
+    for (let li of socialLinks.children){
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  
+    }
+}
+
+
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`   
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        UserName.textContext = data.name
+        userBio.textContext = data.bio
+        userLink.href = data.html_url
+        userImage.src = data.avatar_url
+        userLogin.textContext = data.login
+    })
+} 
+
+
+getGitHubProfileInfos()
